6aa1fabf31186a58f253314a11ed35c46e641bfb
Eddie Atkinson <22487668@student.uwa.edu.au>
2020-07-10 15:27:42 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <div class='bg'>
+        <v-container
+        fill-height
+        ma-1
+        pa-4
+        >
+        <v-row
+        align='center'
+        justify='center'
+        >
+        <v-col
+        align='center'
+        justify='center'
+        >
+          <v-img
+            src='./assets/logo.png'
+            max-width='192px'
+            max-height='192px'
+            class='ma-4'
+            >
+            </v-img>
+          <div class='white--text display-2 ma-3'>
+            New Site Launching 13/07/2020
+          </div>
+        </v-col>
+        </v-row>
+        </v-container>
+    </div>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld,
   },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .bg {
+    width: 100%;
+    height: 100%;
+    background:
+      url('./assets/profile-bg.jpg')
+      no-repeat
+      center
+      center;
+    background-size: cover;
+    transform: scale(1.0);
+  }
 </style>
